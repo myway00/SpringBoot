e97a976bd53225f26ceb41a266440e4a1a202005
myway00 <junior0614@naver.com>
2022-02-09 23:13:43 +0900

    #2 HTML Templates로 데이터 전달 - thyme, jsp

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -3,34 +3,44 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("view")
 public class SimpleController {
     private static final Logger logger
             = LoggerFactory.getLogger(SimpleController.class);
 
-    @RequestMapping(
-            value = "hello"
-    )
-    public String hello(@RequestParam(name="id", required = false, defaultValue = "")String id){
-        logger.info("Path:Hello");
-        logger.info("Query Param id : " + id);
-        return "hello.html";
+    @GetMapping("/simple-jsp")
+    public String simpleJsp(Model model){ //model이라는 데이터 추가
+        logger.info("in simple jsp");
+        //profile arraylist 에 요소들 추가
+        List<SimplePayload> profile = new ArrayList<>();
+        profile.add(new SimplePayload("custard", 23, "developer"));
+        profile.add(new SimplePayload("happy", 22, "teacher"));
+        profile.add(new SimplePayload("luckyHappyPretty", 20, "student"));
+        
+        model.addAttribute("profiles", profile); //전달 요소를 모델에 추가
+        
+        return "view-jsp";
     }
 
-    @GetMapping(
-            value = "/hello/{id}"
-    )
-    public String helloPath(@PathVariable("id") String id){
-        logger.info("Path variable is" + id);
-        return "/hello.html";
-    }
+    @GetMapping("/simple-thyme")
+        public ModelAndView simpleThyme(){
+            ModelAndView mandv = new ModelAndView();
+            logger.info("in simple thyme");
+            List<SimplePayload> profile = new ArrayList<>();
+            profile.add(new SimplePayload("custard", 23, "developer"));
+            profile.add(new SimplePayload("happy", 22, "teacher"));
+            profile.add(new SimplePayload("luckyHappyPretty", 20, "student"));
 
-    @GetMapping(
-            "/get-profile"
-    )
-    public @ResponseBody SimplePayload getProfile(){
-        return new SimplePayload("custard", 23,"Developer");
+            mandv.addObject("profiles",profile);
+            mandv.setViewName("view-thyme");
+            return mandv;
+        }
     }
-}
