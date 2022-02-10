fed1bab292875e1a468c8ffa93f032156e5b901f
myway00 <junior0614@naver.com>
2022-02-10 11:33:11 +0900

    #3 PostMapping, MultiMedia Controller

--- src/main/java/com/example/demo/SimpleRestController.java
+++ src/main/java/com/example/demo/SimpleRestController.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.print.attribute.standard.Media;
 import java.awt.*;
@@ -19,9 +20,34 @@ public class SimpleRestController {
     private static final Logger logger = LoggerFactory.getLogger(SimpleRestController.class);
 
     @GetMapping("/simple-payload")
-    public SimplePayload simplePayload(){
+    public SimplePayload simplePayloadGet(){
         return new SimplePayload("custard", 23,"Developer");
     }
+
+    @PostMapping("/simple-payload")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String simplePayloadPost(@RequestBody SimplePayload simplepayloadata){
+        logger.info(simplepayloadata.toString());
+        return simplepayloadata.getName();
+    }
+
+    @PostMapping(
+            value = "/simple-multipart",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void simpleMultipartpost(
+        @RequestParam("name") String name,
+        @RequestParam("age") Integer age,
+        @RequestParam("occupation") String occupation,
+        @RequestParam("file") MultipartFile multipartFile
+    ){
+        logger.info("name :"+ name);
+        logger.info("age :"+ age);
+        logger.info("o :"+ occupation);
+        logger.info("file name :"+ multipartFile.getOriginalFilename());
+    }
+
 
     @GetMapping(
             value = "/simple-image",
